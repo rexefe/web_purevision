8f4ea3e5273d837b41414bcce7666e6dc17a24eb
Rexefe <rexefe@outlook.com>
2020-02-19 23:30:53 +0100

    channges

--- provison/settings.py
+++ provison/settings.py
@@ -135,7 +135,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'kingrexefe@gmail.com'
-EMAIL_HOST_PASSWORD = 'jesusjesus30'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 
